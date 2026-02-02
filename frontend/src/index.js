7de94d2529eb93f0a950677893b3506480d975be
Sujoy chakravarty <sujoy.crty@gmail.com>
2026-02-02 21:01:06 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css'; // If you have global styles
+//import './index.css'; // If you have global styles
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(

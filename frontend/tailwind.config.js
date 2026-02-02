1f1d54e1e96b9e38f0f0ccf9a5b933f4eca06eae
Sujoy chakravarty <sujoy.crty@gmail.com>
2026-02-02 22:13:39 +0530

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,1 +1,18 @@
-kk
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        quantum: {
+          950: '#020617',
+          400: '#22d3ee',
+        }
+      }
+    },
+  },
+  plugins: [],
+}
